118c288ae50d7de0a9053133783de1fd811ca95a
Erick Silva <erick.silva@firstdecision.com.br>
2021-02-01 10:44:27 -0300

    feat: new logic for logging events, implementation of switch for log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,6 +22,7 @@ let currentMonsterHealth = userMaxLife;
 let currentHeroHealth = userMaxLife;
 let hasBonusLife = true;
 let battleLog = [];
+let lastLog;
 
 adjustHealthBars(userMaxLife);
 
@@ -48,7 +49,29 @@ const healPlayer = () => {
 };
 
 const printLog = () => {
-  console.log(battleLog);
+  for (let i = 0; i < 3; i++) {
+    console.log('//////////');
+  }
+
+  // let j = 0;
+
+  // while (j < 3) {
+  //   console.log('//////////');
+  //   j++;
+  // }
+
+  let i = 0;
+  for (const logEntry of battleLog) {
+    if ((!lastLog && lastLog !== 0) || lastLog < i) {
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLog = i;
+      break;
+    }
+    i++;
+  }
 };
 
 attackBtn.addEventListener('click', atkHandler);
@@ -134,15 +157,33 @@ const writeLog = (event, value, monsterHealth, playerHealth) => {
     playerHealth: playerHealth,
   };
 
-  if (event === LOG_PLAYER_ATK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_PLAYER_STRONG_ATK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_MONSTER_ATK) {
-    logEntry.target = 'PLAYER';
-  } else if (event === LOG_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
+  switch (event) {
+    case LOG_PLAYER_ATK:
+      logEntry.target = 'MONSTER';
+      break;
+
+    case LOG_PLAYER_STRONG_ATK:
+      logEntry.target = 'MONSTER';
+      break;
+
+    case LOG_MONSTER_ATK:
+      logEntry.target = 'PLAYER';
+      break;
+
+    case LOG_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
   }
+
+  // if (event === LOG_PLAYER_ATK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (event === LOG_PLAYER_STRONG_ATK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (event === LOG_MONSTER_ATK) {
+  //   logEntry.target = 'PLAYER';
+  // } else if (event === LOG_PLAYER_HEAL) {
+  //   logEntry.target = 'PLAYER';
+  // }
   battleLog.push(logEntry);
 };
 
